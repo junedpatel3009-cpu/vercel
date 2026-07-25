be696e00ca995561f7c905e9a7f8297bd5bbe02d
junedpatel3009-cpu <junedpatel3009@gmail.com>
2026-07-26 01:47:51 +0530

    fix: inline @asamuzakjp/css-color in vite ssr bundle

--- .tmp-route-type.ts
+++
@@ -1,4 +0,0 @@
-import { Route as IndexRoute } from "./src/routes/index";
-const data = IndexRoute.useLoaderData();
-const check: { openJobs: string } = data;
-void check;
--- .tmp-typecheck.ts
+++
@@ -1,9 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-const getData = createServerFn({ method: "GET" }).handler(async () => ({
-  value: 1,
-  items: [{ id: 1, label: "x" }],
-}));
-type Data = Awaited<ReturnType<typeof getData>>;
-const data: Data = { value: 1, items: [{ id: 1, label: "x" }] };
-const item = data.items[0];
-void item;
